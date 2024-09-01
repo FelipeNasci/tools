26df40d25d1f823dbb55c4478b80860ea581b60d
felipenasci <dfnascimento@live.com>
2024-09-01 16:05:16 -0300

    feat: add readme.md

---
+++ windows/scripts/labs/labinfo01/README.md
@@ -0,0 +1,10 @@
+## Setup Agent
+
+### Add Agent to Windows startup
+1. Download Agent
+
+2. Copy Agent to startup
+- press `win + R`
+- type: shell:startup
+
+3. Paste Agent to startup folder
--- windows/scripts/labs/labinfo01/agent.bat
+++ windows/scripts/labs/labinfo01/agent.bat
@@ -5,11 +5,10 @@ mkdir setup
 cd setup
 
 curl -O -s https://raw.githubusercontent.com/FelipeNasci/tools/main/windows/scripts/labs/labinfo01/script.bat
-dir
 
 call script.bat
 
 cd c://
 rmdir /s /q setup
 
-pause
+#pause
