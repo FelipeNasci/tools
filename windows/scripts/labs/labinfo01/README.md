70760ca8f20d19b6f10dfb9ef8ccfcc65e455acd
felipenasci <dfnascimento@live.com>
2024-09-04 09:00:13 -0300

    feat: enable ps2 devices on windows

--- windows/scripts/labs/labinfo01/README.md
+++ windows/scripts/labs/labinfo01/README.md
@@ -8,3 +8,44 @@
 - type: shell:startup
 
 3. Paste Agent to startup folder
+
+
+### Add Agent by CMD
+
+```bat
+@echo off
+setlocal
+
+
+cd c://
+mkdir setup
+cd setup
+
+:: Download Agent
+curl -O -s https://raw.githubusercontent.com/FelipeNasci/tools/main/windows/scripts/labs/labinfo01/agent.bat
+
+:: Defina o caminho do script que você deseja copiar
+set "source=%~d0\agent.bat"
+
+:: Defina o caminho da pasta de inicialização
+set "destination=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\agent.bat"
+
+:: Copie o script para a pasta de inicialização
+copy "%source%" "%destination%"
+
+cd c://
+rmdir /s /q setup
+
+:: Mensagem de confirmação
+echo Script copiado para a pasta de inicialização com sucesso.
+
+pause
+
+endlocal
+
+:: Reinicie o computador
+echo Reiniciando o computador em 5 segundos...
+
+shutdown -r -t 0
+
+```
--- windows/scripts/labs/labinfo01/script.bat
+++ windows/scripts/labs/labinfo01/script.bat
@@ -26,4 +26,26 @@ for /f "tokens=2 delims==" %%i in ('wmic diskdrive get size /value') do set "STO
 :: Envia os dados para o servidor de eventos
 curl -X POST -H "Content-Type: application/json" -d "{\"name\": \"power-on\", \"unit\": \"MME\", \"place\": \"LABINFO-01\", \"hostName\": \"%HOST_NAME%\", \"meta\": {\"ram\": \"%TOTAL_RAM%\", \"storage\": \"%STORAGE%\", \"cpu\": \"%CPU_NAME%\"}}" https://catch-events.vercel.app/api/host/power-on
 
+:: Script para ativar PS/2 alterando o registro do Windows
+
+:: Verifica se o script está sendo executado como administrador
+net session >nul 2>&1
+if %errorlevel% NEQ 0 (
+    :: Se não estiver sendo executado como administrador, reexecuta o script com privilégios elevados
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\elevate.vbs"
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0""", "", "runas", 1 >> "%temp%\elevate.vbs"
+    "%temp%\elevate.vbs"
+    del "%temp%\elevate.vbs"
+)
+
+:: Altera o valor da chave 'start' para '1'
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\i8042prt" /v Start /t REG_DWORD /d 1 /f
+
+:: Verifica se a alteração foi bem-sucedida
+if %errorlevel% EQU 0 (
+    echo A chave do registro foi alterada com sucesso.
+) else (
+    echo Falha ao alterar a chave do registro.
+)
+
 :: pause
