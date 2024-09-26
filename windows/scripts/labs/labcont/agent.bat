0f7b766b51a8bd53acdce89e06aadb2743ccab96
Diego Felipe <36546304+FelipeNasci@users.noreply.github.com>
2024-09-26 11:44:14 -0300

    Update agent.bat

--- windows/scripts/labs/labcont/agent.bat
+++ windows/scripts/labs/labcont/agent.bat
@@ -4,9 +4,9 @@ cd c://
 mkdir setup
 cd setup
 
-curl -O -s https://raw.githubusercontent.com/FelipeNasci/tools/main/windows/scripts/labs/labcont/run-scripts.bat
+curl -O -s https://raw.githubusercontent.com/FelipeNasci/tools/refs/heads/main/windows/scripts/labs/labcont/run-scripts.bat
 
-run-scripts.bat
+call run-scripts.bat
 
 cd c://
 rmdir /s /q setup
